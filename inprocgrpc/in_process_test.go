0baf001c69853121b7a436b684ec616f36ad984f
Josh Humphries <jh@fullstory.com>
2018-08-14 09:58:53 -0400

    add test that shows goroutines being leaked from unary RPCs that are cancelled or timeout

--- inprocgrpc/in_process_test.go
+++ inprocgrpc/in_process_test.go
@@ -1,7 +1,9 @@
 package inprocgrpc_test
 
 import (
+	"runtime"
 	"testing"
+	"time"
 
 	"github.com/fullstorydev/grpchan/grpchantesting"
 	"github.com/fullstorydev/grpchan/inprocgrpc"
@@ -13,5 +15,20 @@ func TestInProcessChannel(t *testing.T) {
 	var cc inprocgrpc.Channel
 	grpchantesting.RegisterHandlerTestService(&cc, svr)
 
+	before := runtime.NumGoroutine()
+
 	grpchantesting.RunChannelTestCases(t, &cc, true)
+
+	// check for goroutine leaks
+	deadline := time.Now().Add(time.Second * 5)
+	after := 0
+	for deadline.After(time.Now()) {
+		after = runtime.NumGoroutine()
+		if after <= before {
+			// number of goroutines returned to previous level: no leak!
+			return
+		}
+		time.Sleep(time.Millisecond * 50)
+	}
+	t.Errorf("%d goroutines leaked", after-before)
 }
